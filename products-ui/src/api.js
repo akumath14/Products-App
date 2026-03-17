d315225ab2effcf0872bca07ea6c3261b39c0cab
Arpan Kumath <akumath14@Arpans-MacBook-Pro.local>
2026-03-17 17:31:36 -0500

    updated docker

--- products-ui/src/api.js
+++ products-ui/src/api.js
@@ -1,4 +1,4 @@
-const BASE_URL = 'http://localhost:8080/api';
+const BASE_URL = process.env.REACT_APP_API_BASE_URL || '/api';
 
 export const getProducts = async() => {
     const res = await fetch(`${BASE_URL}/products`);
